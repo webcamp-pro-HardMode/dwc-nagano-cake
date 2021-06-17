67d2cde84ac18b149902f6c1568998d4ba9d1222
seina25 <replace421@gmail.com>
2021-06-17 14:42:50 +0000

    [Add]管理者ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :customers
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  # 顧客
+   devise_for :customers
 
   scope module: :public do
     root to:  'homes#top'
@@ -23,7 +23,14 @@ Rails.application.routes.draw do
   end
 
   # 管理者
-   get '/admin' => 'admin/homes#top'
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  get '/admin' => 'admin/homes#top'
+
   namespace :admin do
     resources :items, only:  [ :index, :new, :create, :show, :update, :edit]
     resources :genres, only:  [ :index, :create, :update, :edit]
