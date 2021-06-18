aca380ab5a1a245384f27625b0865b45e5ef6b5c
seina25 <replace421@gmail.com>
2021-06-18 15:05:10 +0000

    [Add]管理者側の顧客情報詳細と編集機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,17 +1,21 @@
 class Admin::CustomersController < ApplicationController
-  before_action :authenticate_customer!
+  before_action :authenticate_admin!
   def index
   end
 
   def show
-    #@customer = current_customer
+    #条件に合致するオブジェクトを配列として取り出しeach文に渡すためwhereメソッドを使用
+    @customer = Customer.where(id: params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
-
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
 
   private
@@ -19,6 +23,5 @@ class Admin::CustomersController < ApplicationController
   def customer_params
       params.require(:customer).permit(:first_name, :family_name, :kana_sei, :kama_mei, :email, :postal_code, :address, :phone_number)
   end
-
 
 end
