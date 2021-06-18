4d8f810988500b798a477ba9922de3350ebe0471
seina25 <replace421@gmail.com>
2021-06-18 12:37:24 +0000

    [Add]顧客詳細・編集機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,10 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_customer!
   def index
   end
 
   def show
+    #@customer = current_customer
   end
 
   def edit
@@ -10,6 +12,12 @@ class Admin::CustomersController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def customer_params
+      params.require(:customer).permit(:first_name, :family_name, :kana_sei, :kama_mei, :email, :postal_code, :address, :phone_number)
   end
 
 
