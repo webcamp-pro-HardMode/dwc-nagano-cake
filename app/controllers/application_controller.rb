967d117293e0c836b04496cea356b47434efaf30
seina25 <replace421@gmail.com>
2021-06-20 01:10:40 +0000

    [Add]退会機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #before_action :reject_inactive_customer, only:[:new, :create]
 
   #ログイン後の遷移先
   def after_sign_in_path_for(resource)
@@ -7,7 +8,7 @@ class ApplicationController < ActionController::Base
     when Admin
       orders_path
     when Customer
-      customers_my_page_path
+      my_page_path
     end
   end
 
@@ -25,6 +26,18 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :kana_sei, :kana_mei, :postal_code, :address, :phone_number])
+  end
+
+  private
+
+  #退会したエンドユーザーのログインを阻止する
+  def reject_inactive_customr
+    @customer = Customer.find_by(name: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@customer.is_valid
+        redirect_to new_customer_session_path
+      end
+    end
   end
 
 
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,20 +3,31 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
-  def change
-  end
-
   def edit
     @customer = current_customer
   end
 
   def update
     @customer = current_customer
-    @customer.update
-    redirect_to customers_my_page_path
+    @customer.update(customer_params)
+    redirect_to my_page_path
   end
 
+  #退会確認画面
+  def change
+    @customer = current_customer
+  end
+
+  #退会機能
   def withdraw
+    @customer = current_customer
+    #trueで更新する（true：退会、false：有効）
+    @customer.update(is_exist: true)
+    #sessionメソッドでログイン時の情報を削除（メール・パスワード）、ログアウトさせる
+    #application_controllerに定義する
+    reset_session
+    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+    redirect_to root_path
   end
 
   private
