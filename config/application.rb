6a93af7f678ac552c4291ba465c09bb5db3e7347
seina25 <replace421@gmail.com>
2021-06-24 01:29:36 +0000

    [エラー分追加]

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,7 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone (:local)
+
+    config.i18n.default_locale = :ja
   end
 end
