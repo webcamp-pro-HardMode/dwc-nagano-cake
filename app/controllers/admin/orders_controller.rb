17ff498a98c6bb45626c320b6bd6406bfc5d0f3d
P1zza-L1cht <haruki21corder@gmail.com>
2021-06-26 01:17:27 +0000

    [add]layout

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,10 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_items = @order.order_items
+    @total = 0
+    @order_items.each do |order_item| 
+      @total += (order_item.item.price * 1.1).round(-1) * order_item.quantity
+    end
     # binding.pry
   end
 
