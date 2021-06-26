d25ad98a70dbd56b6796a85223e1ce6bcf7ff3de
seina25 <replace421@gmail.com>
2021-06-26 01:31:41 +0000

    [Update]アクセス権限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # deviseにまつわる画面に行った時に全ての画面でconfigure_permitted_parametersが起動
   before_action :configure_permitted_parameters , if: :devise_controller?
-
+  
   #ログイン後の遷移先
   def after_sign_in_path_for(resource)
     case resource
